0a0dca392859ad482be94d5b8e1fe3f6ec8d82a1
arvind-balaji <arvindb02@gmail.com>
2019-03-22 23:46:48 -0500

    Implemented Card API

--- components/card/controller.js
+++ components/card/controller.js
@@ -19,15 +19,36 @@ module.exports = {
   },
 
   // ctxt = {id}
-  get: async (ctx) =>{
+  get: async (ctxt) =>{
+    const {id} = ctxt
+    try {
+      const card = await Card.findOne({_id:id});
+      return card
+    } catch (error) {
+      throw error;
+    }
+  },
 
+  // ctxt = {skip, limit, query}
+  getAll: async (ctxt) => {
+    const {skip=0, limit=100} = ctxt;
+    try {
+      const cards = await Card
+      .find({})
+      .lean()
+      .skip(skip)
+      .limit(limit);
+      return cards
+    } catch (error) {
+      throw error;
+    }
   },
 
   // ctxt = {id}
   delete: async (ctxt) => {
     const {id} = ctxt
     try {
-      const card = await File.findOne({id});
+      const card = await Card.findOne({id});
       await Card.deleteOne({id});
       return card;
     } catch (error) {
