77badd7024388bf38f19f9c773a7e5abf53b0ebb
D0ugins <60764137+D0ugins@users.noreply.github.com>
2022-03-01 22:11:24 -0600

    refactor: rewrite card extractor (#10)
    * Rewrite documentToTokens to use document.xml
    Since extracting text with basic formatting is relatively simple, instead of using external libraries, it is much faster to just unzip the .docx file and parse the document.xml from scratch
    
    * Rewrite tokensToMarkup to use string
    
    Since the tokenizer is much faster, tokensToMarkup became the bottleneck in extracting cards, rewritting to use a string instead of cheerio dom speeds up card extraction by 3-4x
    
    * Fix formatting
    
    * Fix incorrect formatting in some documents
    Some text tokens overwrite styles set by the style name, which wasnt handled properly
    
    * Fix detection of headings on some documents
    Sometimes documents will use the outlinelvl property instead of the heading style to indicate a heading, this is now handled properly

--- src/lib/debate-tools/styles.ts
+++ src/lib/debate-tools/styles.ts
@@ -1,58 +1,66 @@
 import { HeadingLevel, IParagraphOptions, IRunOptions } from 'docx';
 import { findKey, pickBy } from 'lodash';
+import { TokenStyle } from '.';
 
 interface Style {
   block: boolean;
   heading: boolean;
   domSelector: string[];
   domElement: string;
+  xmlName?: string;
   docxStyles?: IParagraphOptions | IRunOptions;
 }
 
-export type StyleName = 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'underline' | 'strong' | 'mark';
+export type SectionStyleName = 'pocket' | 'hat' | 'block' | 'tag' | 'text';
+export type TokenStyleName = 'underline' | 'strong' | 'mark';
+export type StyleName = SectionStyleName | TokenStyleName;
 
 export const styleMap: Record<StyleName, Style> = {
-  h1: {
+  pocket: {
     block: true,
     heading: true,
     domSelector: ['h1'],
     domElement: 'h1',
+    xmlName: 'Heading1',
     docxStyles: {
       heading: HeadingLevel.HEADING_1,
       outlineLevel: 1,
     },
   },
-  h2: {
+  hat: {
     block: true,
     heading: true,
     domSelector: ['h2'],
     domElement: 'h2',
+    xmlName: 'Heading2',
     docxStyles: {
       heading: HeadingLevel.HEADING_2,
       outlineLevel: 2,
     },
   },
-  h3: {
+  block: {
     block: true,
     heading: true,
     domSelector: ['h3'],
     domElement: 'h3',
+    xmlName: 'Heading3',
     docxStyles: {
       heading: HeadingLevel.HEADING_3,
       outlineLevel: 3,
     },
   },
-  h4: {
+  tag: {
     block: true,
     heading: true,
     domSelector: ['h4'],
     domElement: 'h4',
+    xmlName: 'Heading4',
     docxStyles: {
       heading: HeadingLevel.HEADING_4,
       outlineLevel: 4,
     },
   },
-  p: {
+  text: {
     block: true,
     heading: false,
     domSelector: ['p'],
@@ -87,16 +95,22 @@ export const styleMap: Record<StyleName, Style> = {
   },
 };
 
-export const getStyleByElement = (elementName: StyleName): StyleName => {
-  const predicate = ({ domSelector }) => domSelector.includes(elementName);
-  return findKey(styleMap, predicate) as StyleName;
+export const getStyleNameByXml = (elXmlName: string): SectionStyleName => {
+  const predicate = ({ xmlName = null }) => elXmlName === xmlName;
+  return (findKey(styleMap, predicate) ?? 'text') as SectionStyleName;
+};
+
+export const getStyleNameByOutlineLvl = (outlineLvl: number): SectionStyleName => {
+  const predicate = ({ docxStyles = null }) => outlineLvl === docxStyles?.outlineLevel;
+  return (findKey(styleMap, predicate) ?? 'text') as SectionStyleName;
 };
 
 export const getStyles = (filter: Partial<Style>): StyleName[] => {
   return Object.keys(pickBy(styleMap, filter)) as StyleName[];
 };
 
-export const getDocxStyles = (styleNames: StyleName[]): IParagraphOptions | IRunOptions => {
-  const mergedStyles = styleNames.reduce((acc, key) => ({ ...acc, ...styleMap[key]?.docxStyles }), {});
+export const getDocxStyles = (styleNames: TokenStyle): IParagraphOptions | IRunOptions => {
+  const styles = pickBy(styleNames, (el) => el); // Get only styles that are set as true
+  const mergedStyles = Object.keys(styles).reduce((acc, key) => ({ ...acc, ...styleMap[key]?.docxStyles }), {});
   return mergedStyles;
 };
