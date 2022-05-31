ed763dad5335ef72e84f925cb1ce528071bfa8f6
D0ugins <yusufishabazz@gmail.com>
2022-05-30 23:33:34 -0500

    Fix failed upserts on evidenceBuckets

--- src/modules/deduplicator/index.ts
+++ src/modules/deduplicator/index.ts
@@ -1,10 +1,11 @@
-import { db, Children } from 'app/lib';
+import { db, Children, Lock } from 'app/lib';
 import { findParent } from 'app/lib/debate-tools';
 import { CONCURRENT_DEDUPLICATION } from 'app/constants';
 import { onAddEvidence } from 'app/actions/addEvidence';
 import { Queue } from 'typescript-collections';
 
 const evidenceQueue = new Queue<{ gid: string }>();
+const updateLock: Record<number, Lock> = {};
 
 // Update parents in database and redis
 async function updateParents(cardIds: string[], parentId: number) {
@@ -26,12 +27,16 @@ const drain = async () => {
   // TODO: Add chunks of unduplicated cards from db if queue is empty
   try {
     if (evidenceQueue.size() === 0) return setTimeout(drain, 1000);
-    else {
-      const { gid } = evidenceQueue.dequeue();
-      const { id, fulltext } = await db.evidence.findUnique({ where: { gid }, select: { id: true, fulltext: true } });
-      const { updates, parent } = await findParent(id, fulltext);
-      await updateParents(updates, parent);
-    }
+    const { gid } = evidenceQueue.dequeue();
+    const { id, fulltext } = await db.evidence.findUnique({ where: { gid }, select: { id: true, fulltext: true } });
+    const { updates, parent } = await findParent(id, fulltext);
+
+    // Only wait if it actually exists, otherwise it wont get set in time
+    if (updateLock[parent]) await updateLock[parent].promise;
+    const lock = (updateLock[parent] = new Lock());
+    await updateParents(updates, parent);
+    lock.unlock();
+    if (updateLock[parent] === lock) delete updateLock[parent];
   } catch (e) {
     console.error(e);
   }
