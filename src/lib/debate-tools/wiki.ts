b3955a53e62e45e0899f6c43ad664666fdcafe6c
D0ugins <yusufishabazz@gmail.com>
2022-06-01 16:13:26 -0500

    Add strongly typed api responses

--- src/lib/debate-tools/wiki.ts
+++ src/lib/debate-tools/wiki.ts
@@ -1,6 +1,6 @@
-import { wikiRequest } from 'app/lib';
-import { WIKITYPES, WIKI_NAME_REGEX } from 'app/constants';
-import type { Prisma } from '@prisma/client';
+import { wikiRequest } from 'app/lib/request';
+import { WIKITYPES, WIKI_NAME_REGEX, CITE, OBJECT_SUMMARIES, ROUND, SPACES, WIKIS, ATTACHMENTS } from 'app/constants';
+import type { Prisma, Side } from '@prisma/client';
 import { keyBy, lowerFirst, mapValues, pick } from 'lodash';
 
 export interface WikiInfo {
@@ -20,15 +20,15 @@ const getCites = async (pageUrl: string, citeNums: string): Promise<string[]> =>
   const cites = [];
   for (const num of citeNums.split(',')) {
     if (!num) continue;
-    const cite = await wikiRequest(`${pageUrl}/objects/Caselist.CitesClass/${num}/properties/Cites`);
+    const cite = await wikiRequest(`${pageUrl}/objects/Caselist.CitesClass/${num}/properties/Cites`, CITE);
     if (!('err' in cite)) cites.push(cite.value);
   }
   return cites;
 };
 
 export const loadRound = async (pageUrl: string, roundId: number): Promise<Omit<Prisma.RoundCreateInput, 'gid'>> => {
-  const raw = await wikiRequest(`${pageUrl}/objects/Caselist.RoundClass/${roundId}`);
-  if ('err' in raw) throw new Error(`Error loading round ${pageUrl} / ${roundId}: ${raw.err.message}`);
+  const raw = await wikiRequest(`${pageUrl}/objects/Caselist.RoundClass/${roundId}`, ROUND);
+  if ('err' in raw) throw new Error(raw.err.message);
 
   const properties = mapValues(
     keyBy(raw.properties, ({ name }) => lowerFirst(name)),
@@ -49,7 +49,7 @@ export const loadRound = async (pageUrl: string, roundId: number): Promise<Omit<
     : undefined;
 
   return {
-    ...{ wiki, school, team, side, cites, roundNum },
+    ...{ wiki, school, team, side: side as Side, cites, roundNum },
     ...pick(properties, 'tournament', 'judge', 'opponent', 'roundReport'),
     entryDate: new Date(properties.entryDate),
     openSourceUrl,
@@ -70,25 +70,26 @@ const parseWikiName = (name: string): WikiInfo => {
 
 type WikiLoader = () => Promise<WikiInfo[]>;
 export const loadOpenEvs: WikiLoader = async () => {
-  const spaceData = (await wikiRequest('https://openev.debatecoaches.org/rest/wikis/openev/spaces')).spaces;
-  if (!spaceData) throw new Error('Failed to load openev data');
+  const spaceData = await wikiRequest('https://openev.debatecoaches.org/rest/wikis/openev/spaces', SPACES);
+  if ('err' in spaceData) throw new Error('Failed to load openev data: ' + spaceData.err.message);
 
-  return spaceData.filter((space) => space.name.startsWith('20')).map(({ name }) => parseWikiName('openev' + name));
+  return spaceData.spaces
+    .filter((space) => space.name.startsWith('20'))
+    .map(({ name }) => parseWikiName('openev' + name));
 };
 
 export const loadWikis: WikiLoader = async () => {
-  const wikiData = (await wikiRequest(`https://openev.debatecoaches.org/rest/wikis/`)).wikis;
-  if (!wikiData) throw new Error('Failed to load wikis');
+  const wikiData = await wikiRequest(`https://openev.debatecoaches.org/rest/wikis/`, WIKIS);
+  if ('err' in wikiData) throw new Error('Failed to load wikis: ' + wikiData.err.message);
 
-  return wikiData.filter((wiki) => wiki.name !== 'openev').map(({ name }) => parseWikiName(name));
+  return wikiData.wikis.filter((wiki) => wiki.name !== 'openev').map(({ name }) => parseWikiName(name));
 };
 
 const base = 'https://openev.debatecoaches.org/rest/wikis/';
 export const loadRounds = async (wiki: WikiInfo): Promise<RoundInfo[]> => {
-  const rounds = (await wikiRequest(`${base}/${wiki.name}/classes/Caselist.RoundClass/objects`)).objectSummaries;
-  if (!rounds) throw new Error('Could not load data for ' + wiki.name);
-
-  return rounds.map((round) => ({
+  const rounds = await wikiRequest(`${base}/${wiki.name}/classes/Caselist.RoundClass/objects`, OBJECT_SUMMARIES);
+  if ('err' in rounds) throw new Error(`Could not load data for ${wiki.name}: ${rounds.err.message}`);
+  return rounds.objectSummaries.map((round) => ({
     url: round.links[0].href.split('/').slice(0, -3).join('/'),
     roundId: round.number,
     gid: round.guid,
@@ -96,7 +97,11 @@ export const loadRounds = async (wiki: WikiInfo): Promise<RoundInfo[]> => {
 };
 
 export const loadOpenEv = async (wiki: WikiInfo): Promise<{ name: string; url: string; wiki: WikiInfo }[]> => {
-  const attachments = (await wikiRequest(`${base}/openev/spaces/${wiki.year}/attachments?number=-1`)).attachments;
-  if (!attachments) throw new Error('Could not load data for ' + wiki.name);
-  return attachments.map((attachment) => ({ name: attachment.name, url: attachment.links[1].href, wiki }));
+  const attachments = await wikiRequest(`${base}/openev/spaces/${wiki.year}/attachments?number=-1`, ATTACHMENTS);
+  if ('err' in attachments) throw new Error(`Could not load data for ${wiki.name}: ${attachments.err.message}`);
+  return attachments.attachments.map(({ name, xwikiAbsoluteUrl }) => ({
+    name,
+    url: xwikiAbsoluteUrl,
+    wiki,
+  }));
 };
