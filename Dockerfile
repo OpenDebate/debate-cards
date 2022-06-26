522891a68d1dc1df638830038dec6e48530045c9
D0ugins <yusufishabazz@gmail.com>
2022-06-25 23:07:47 -0500

    Containerize app
    Currently designed for development only.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Designed for development, for production you would copy the built files from this container
+FROM node:18
+
+WORKDIR /app
+
+COPY package*.json .
+RUN yarn
+
+COPY prisma prisma
+COPY .env .
+RUN npx prisma generate
+
+COPY tsconfig.json .
+COPY src src
+
+RUN yarn run build
+CMD ["yarn", "run", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import addFile from 'app/actions/addFile';
-// import generateFile from 'app/actions/generateFile';
+import generateFile from 'app/actions/generateFile';
 import 'app/modules/parser';
 import 'app/modules/deduplicator';
 import { db } from './lib';
@@ -11,42 +11,23 @@ async function loadDir(dir: string) {
     path: `${dir}/${file}`,
   }));
 
-  // for (const file of files) await addFile(file);
+  for (const file of files) await addFile(file);
 }
 
-// async function makeFile(id: number) {
-//   const ids = (
-//     await db.evidence.findMany({
-//       where: { fileId: id },
-//     })
-//   ).map((card) => card.id);
-//   const file = await generateFile(ids, true);
-//   await writeFile('./test.docx', file);
-//   console.log('File built');
-// }
+async function makeFile(id: number) {
+  const ids = (
+    await db.evidence.findMany({
+      where: { fileId: id },
+    })
+  ).map((card) => card.id);
+  const file = await generateFile(ids, true);
+  await writeFile('./test.docx', file);
+  console.log('File built');
+}
 
 (async () => {
   try {
-    // wiki.main();
-    // dedup
-    const files = await readdir('/Users/arvindb/Downloads/2020OpenEv');
-    for (const file of files) {
-      await addFile({
-        name: file,
-        path: `/Users/arvindb/Downloads/2020OpenEv/${file}`,
-      });
-    }
-    // const ev = await db.evidence.findMany({
-    //   where: {
-    //     fileId: 1,
-    //   },
-    // });
-    // console.log(ev.length);
-    // const res = await generateFile(
-    //   ev.map((e) => e.id),
-    //   true,
-    // );
-    // await writeFile('./test.docx', res);
+    console.log('Started');
   } catch (error) {
     console.error(error);
   }
