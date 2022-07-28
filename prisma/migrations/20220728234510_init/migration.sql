f5076d7df6c85025e8025b48b1e57f06309cd112
D0ugins <yusufishabazz@gmail.com>
2022-07-28 18:51:25 -0500

    Update migrations

--- prisma/migrations/20220728234510_init/migration.sql
+++ prisma/migrations/20220728234510_init/migration.sql
@@ -1,7 +1,4 @@
 -- CreateEnum
-CREATE TYPE "Side" AS ENUM ('AFF', 'NEG');
-
--- CreateEnum
 CREATE TYPE "FileStatus" AS ENUM ('PENDING', 'PROCESSING', 'PROCESSED', 'ERROR');
 
 -- CreateTable
@@ -36,30 +33,6 @@ CREATE TABLE "Evidence" (
 );
 
 -- CreateTable
-CREATE TABLE "Round" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "gid" TEXT NOT NULL,
-    "status" "FileStatus" NOT NULL DEFAULT E'PENDING',
-    "wiki" TEXT NOT NULL,
-    "school" TEXT NOT NULL,
-    "team" TEXT NOT NULL,
-    "side" "Side" NOT NULL,
-    "entryDate" TIMESTAMP(3) NOT NULL,
-    "tournament" TEXT NOT NULL,
-    "roundNum" TEXT NOT NULL,
-    "opponent" TEXT NOT NULL,
-    "judge" TEXT NOT NULL,
-    "roundReport" TEXT,
-    "cites" TEXT[],
-    "openSourceUrl" TEXT,
-    "openSourceId" TEXT,
-
-    CONSTRAINT "Round_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "File" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -67,7 +40,7 @@ CREATE TABLE "File" (
     "gid" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
-    "status" "FileStatus" NOT NULL DEFAULT E'PENDING',
+    "status" "FileStatus" NOT NULL DEFAULT 'PENDING',
     "evidenceSetId" INTEGER,
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
@@ -103,25 +76,16 @@ CREATE UNIQUE INDEX "EvidenceBucket_rootId_key" ON "EvidenceBucket"("rootId");
 CREATE UNIQUE INDEX "Evidence_gid_key" ON "Evidence"("gid");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Round_gid_key" ON "Round"("gid");
-
--- CreateIndex
 CREATE UNIQUE INDEX "File_gid_key" ON "File"("gid");
-
--- CreateIndex
-CREATE UNIQUE INDEX "EvidenceSet_name_key" ON "EvidenceSet"("name");
 
 -- AddForeignKey
 ALTER TABLE "EvidenceBucket" ADD CONSTRAINT "EvidenceBucket_rootId_fkey" FOREIGN KEY ("rootId") REFERENCES "Evidence"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Evidence" ADD CONSTRAINT "Evidence_bucketId_fkey" FOREIGN KEY ("bucketId") REFERENCES "EvidenceBucket"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Evidence" ADD CONSTRAINT "Evidence_fileId_fkey" FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Round" ADD CONSTRAINT "Round_openSourceId_fkey" FOREIGN KEY ("openSourceId") REFERENCES "File"("gid") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Evidence" ADD CONSTRAINT "Evidence_bucketId_fkey" FOREIGN KEY ("bucketId") REFERENCES "EvidenceBucket"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "File" ADD CONSTRAINT "File_evidenceSetId_fkey" FOREIGN KEY ("evidenceSetId") REFERENCES "EvidenceSet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
