8d9f0b33731e23a1796022d8ce97311679477b24
D0ugins <yusufishabazz@gmail.com>
2022-08-17 11:37:10 -0400

    Improve tags for openev files

--- src/actions/addOpenev.ts
+++ src/actions/addOpenev.ts
@@ -9,17 +9,21 @@ export default async (data: ModelFile): Promise<void> => {
   const campName = CAMP_NAMES[data.camp] ?? data.camp;
   const tags = [
     connectOrCreateTag('openev', 'Open Evidence'),
+    connectOrCreateTag('hs', 'High school'),
     connectOrCreateTag(data.year.toString(), data.year.toString()),
     connectOrCreateTag(data.camp, `${campName} Camp`),
   ];
-  if (data.lab) tags.push(connectOrCreateTag(data.lab, `${data.year} ${campName} ${data.lab} Lab`));
+  if (data.lab)
+    tags.push(connectOrCreateTag(`${data.year}${data.camp}:${data.lab}`, `${data.year} ${campName} ${data.lab} Lab`));
   // Sent as strigified object for some reason
   if (data.tags)
     for (const tag in JSON.parse(data.tags)) {
       // Some old ones had imp instead of impact
       const fixedTag = tag === 'imp' ? 'impact' : tag;
-      tags.push(connectOrCreateTag(fixedTag, `Openev ${OPENEV_TYPES[fixedTag] ?? tag}`));
+      const label = tag === 'ld' ? 'Lincoln Douglas' : `Openev ${OPENEV_TYPES[fixedTag] ?? tag}`;
+      tags.push(connectOrCreateTag(fixedTag, label));
     }
+  if (!tags.find((tag) => tag.where.name === 'ld')) tags.push(connectOrCreateTag('cx', 'Policy'));
 
   return addFile({
     name: data.name,
