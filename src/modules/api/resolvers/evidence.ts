39c1bee5f007b1dafa8ad6968ab9a1c01febe156
D0ugins <yusufishabazz@gmail.com>
2022-08-19 12:23:52 -0400

    Improve organization of query code

--- src/modules/api/resolvers/evidence.ts
+++ src/modules/api/resolvers/evidence.ts
@@ -13,15 +13,40 @@ const EvidenceGetResolver = createGetResolver('evidence', Evidence);
 
 @Resolver()
 export class EvidenceResolver extends EvidenceGetResolver {
-  // A lot of things are hard coded at the moment, in the future could be made customizable
   @Query((returns) => [Evidence])
   async search(
     @Args() { query, size, fields, tags, duplicateWeight }: EvidenceSearchArgs,
     @Info() info: GraphQLResolveInfo,
   ): Promise<Partial<Evidence>[]> {
+    const searchQuery = {
+      query_string: {
+        query,
+        type: 'most_fields', // sums scores from each field
+        fields: fields.map(({ field, weight }) => `${SearchableEvidenceField[field]}^${weight}`),
+      },
+    } as const;
+    const duplicateBoost = {
+      script_score: {
+        script: {
+          params: { duplicateWeight },
+          source: "_score * Math.pow(doc['duplicateCount'].value, params.duplicateWeight)",
+        },
+      },
+    } as const;
+    const filter = tags
+      ? {
+          terms_set: {
+            'File.Tags': {
+              terms: tags.map((tag) => tag.toLowerCase()), // For some reason only lowercase works
+              // Number of tags matched has to equal number of tags in query
+              minimum_should_match_script: { params: { numTags: tags.length }, source: 'params.numTags' },
+            },
+          },
+        }
+      : undefined;
+
     // Does a search in elastic that only returns ids, then queries those ids from postgres
     // Not sure if this is the best approach but otherwise it would be more difficult to do relational queries
-    console.time('search');
     const results = await elastic.search({
       index: 'evidence',
       size,
@@ -29,53 +54,21 @@ export class EvidenceResolver extends EvidenceGetResolver {
         bool: {
           must: {
             function_score: {
-              query: {
-                query_string: {
-                  query,
-                  type: 'most_fields', // sums scores from each field
-                  fields: fields.map(({ field, weight }) => `${SearchableEvidenceField[field]}^${weight}`),
-                },
-              },
-              functions: [
-                {
-                  script_score: {
-                    script: {
-                      params: { duplicateWeight },
-                      source: "_score * Math.pow(doc['duplicateCount'].value, params.duplicateWeight)",
-                    },
-                  },
-                },
-              ],
+              query: searchQuery,
+              functions: [duplicateBoost],
               boost_mode: 'replace',
             },
           },
-          filter: tags
-            ? {
-                terms_set: {
-                  'File.Tags': {
-                    terms: tags.map((tag) => tag.toLowerCase()), // For some reason only lowercase works
-                    // Number of tags matched has to equal number of tags in query
-                    minimum_should_match_script: {
-                      params: { numTags: tags.length },
-                      source: 'params.numTags',
-                    },
-                  },
-                },
-              }
-            : undefined,
+          filter,
         },
       },
-      collapse: {
-        field: 'bucketId',
-      },
+      collapse: { field: 'bucketId' },
       _source: false,
       docvalue_fields: ['id'],
     });
-    console.timeEnd('search');
+
     const ids: number[] = flatMap(results.hits.hits, 'fields.id');
-    console.time('load');
     const evidence = await db.evidence.findMany({ where: { id: { in: ids } }, select: selectFields(info) });
-    console.timeEnd('load');
     // Resort results based on ranking
     return evidence.sort((a: { id: number }, b: { id: number }) => ids.indexOf(a.id) - ids.indexOf(b.id));
   }
