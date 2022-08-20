616e460b2187323a58ae10f8e03365854b9611d9
D0ugins <yusufishabazz@gmail.com>
2022-08-19 20:44:04 -0400

    Fix file parsing erorr handling

--- src/lib/debate-tools/document.ts
+++ src/lib/debate-tools/document.ts
@@ -7,12 +7,12 @@ import fs from 'fs';
   1 - open document.xml and styles.xml by unzipping .docx file
   2 - tokenize document.xml and pull info on named styles from styles.xml
 */
-export const documentToTokens = async (filepath: string): Promise<TextBlock[]> => {
-  const document = loadXml(filepath, /document\.xml$/);
-  const styles = loadXml(filepath, /styles\.xml$/);
-  const tokens = await markupToTokens(document, styles, { simplified: true });
-  return tokens;
-};
+export const documentToTokens = (filepath: string): Promise<TextBlock[]> =>
+  new Promise((resolve, reject) => {
+    const document = loadXml(filepath, /document\.xml$/).on('error', reject);
+    const styles = loadXml(filepath, /styles\.xml$/).on('error', reject);
+    markupToTokens(document, styles, { simplified: true }).then(resolve).catch(reject);
+  });
 
 /* 
   1 - open document.xml
@@ -26,5 +26,4 @@ export const documentToMarkup = async (filepath: string): Promise<string> => {
 };
 
 // Load xml by unzipping docx file, file is regex for file name to look for
-const loadXml = (path: string, file: RegExp) =>
-  fs.createReadStream(path).pipe(ParseOne(file)).on('error', console.error);
+const loadXml = (path: string, file: RegExp) => fs.createReadStream(path).pipe(ParseOne(file));
