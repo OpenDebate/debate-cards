c960dd099a022cf640b1abc85af2c952cf25d910
D0ugins <yusufishabazz@gmail.com>
2022-09-25 12:18:54 -0400

    Add querying of files by tag

--- src/modules/api/resolvers/file.ts
+++ src/modules/api/resolvers/file.ts
@@ -1,8 +1,37 @@
+import { db } from 'app/lib';
+import { selectFields } from 'app/lib/graphql';
+import { GraphQLResolveInfo } from 'graphql';
 import 'reflect-metadata';
-import { Resolver } from 'type-graphql';
+import { Args, ArgsType, Field, Info, Query, Resolver } from 'type-graphql';
 import { createGetResolver } from '.';
 import { File } from '../models';
 
+@ArgsType()
+class TagFilesInput {
+  @Field((type) => [String], {
+    nullable: true,
+    defaultValue: [],
+    description: 'Files must match every one of these tags',
+  })
+  every?: string[];
+
+  @Field((type) => [String], {
+    nullable: true,
+    defaultValue: [],
+    description: 'Files must match at least one of these tags',
+  })
+  some?: string[];
+}
+const tagSelect = (tags: string[]) => tags?.map((tag) => ({ tags: { some: { name: tag } } }));
+
 const FileGetResolver = createGetResolver('file', File);
 @Resolver()
-export class FileResolver extends FileGetResolver {}
+export class FileResolver extends FileGetResolver {
+  @Query((returns) => [File], { nullable: true })
+  async tagFiles(@Args() { every, some }: TagFilesInput, @Info() info: GraphQLResolveInfo): Promise<Partial<File>[]> {
+    return db.file.findMany({
+      where: { AND: tagSelect(every), OR: tagSelect(some) },
+      select: selectFields(info),
+    });
+  }
+}
