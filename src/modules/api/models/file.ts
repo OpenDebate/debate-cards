c7ba60a19a44608e56d0c27cf06cc5943930942b
D0ugins <yusufishabazz@gmail.com>
2022-09-28 18:18:10 -0400

    Add round field to file in api

--- src/modules/api/models/file.ts
+++ src/modules/api/models/file.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { Field, ID, ObjectType } from 'type-graphql';
 import { File as FileSchema } from '@prisma/client';
-import { Tag, Evidence } from '.';
+import { Tag, Evidence, Round } from '.';
 
 @ObjectType()
 export class File implements Partial<FileSchema> {
@@ -13,6 +13,9 @@ export class File implements Partial<FileSchema> {
 
   @Field((type) => [Evidence])
   evidence: Evidence[];
+
+  @Field((type) => Round, { nullable: true })
+  round?: Round;
 
   @Field((type) => [Tag])
   tags: Tag[];
