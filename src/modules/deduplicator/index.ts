93991778170fbb8eed7e45cc5634e0b009fa0061
D0ugins <yusufishabazz@gmail.com>
2022-11-01 20:08:31 -0400

    Improve dedup queue performance Setting a value for take makes the results be returned in order This reduces contention because cards from the same file are less likley to be duplicates

--- src/modules/deduplicator/index.ts
+++ src/modules/deduplicator/index.ts
@@ -14,8 +14,15 @@ export default {
       let tasks: { gid: string }[] = [];
       if (gids) tasks = tasks.concat(gids.map((gid) => ({ gid })));
       if (loadPending)
-        tasks = tasks.concat(await db.evidence.findMany({ where: { bucketId: null }, select: { gid: true }, take }));
-
+        tasks = tasks.concat(
+          await db.evidence.findMany({
+            where: { bucketId: null },
+            select: { gid: true },
+            // Setting a value for take makes the results be returned in order
+            // This reduces contention because cards from the same file are less likley to be duplicates
+            take: take ?? 1000 * 1000 * 1000,
+          }),
+        );
       return tasks;
     },
   ),
