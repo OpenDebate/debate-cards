0fa818cae0a3d53bc9ea613a51f1732013712b74
D0ugins <yusufishabazz@gmail.com>
2023-01-02 23:09:17 -0500

    Fix quote regex

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -24,7 +24,7 @@ export const SHOULD_MERGE = (matching: number, total: number): boolean => matchi
 */
 export const SENTENCE_REGEX = /([.?!])+(?=\d*\s+[A-Z])/;
 // Matches text inside quotes
-export const QUOTE_REGEX = /["“”]([^"]*)["“”]/g;
+export const QUOTE_REGEX = /["“”]([^"“”]*)["“”]/g;
 
 // Wait between requests to caselist api in ms. Seems to be limited to 1500 requests every 15 minutes
 export const REQUEST_WAIT = 1000;
